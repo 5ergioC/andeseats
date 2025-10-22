ea6aa69538f3bedd429fb4454f7e931dfbe6c2b8
Sergio Alejandro Castaño Arcila <sa.castanoa1@uniandes.edu.co>
2025-10-21 22:22:07 -0500

    cambio a .env para las llaves

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,41 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAvvci6hnRBv7upjG-SdhMBqkXMJPCHiTI",
-    authDomain: "andeseats.firebaseapp.com",
-    projectId: "andeseats",
-    storageBucket: "andeseats.appspot.com",
-    messagingSenderId: "687027400186",
-    appId: "1:687027400186:web:44b8bfeaf1cde7d3204843",
-    measurementId: "G-MLNX5C18S3"
+const envSources = {
+    apiKey: ['REACT_APP_FIREBASE_API_KEY'],
+    authDomain: ['REACT_APP_FIREBASE_AUTH_DOMAIN', 'REACT_APP_FIREBASE_DOMAIN'],
+    projectId: ['REACT_APP_FIREBASE_PROJECT_ID'],
+    storageBucket: ['REACT_APP_FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: ['REACT_APP_FIREBASE_MESSAGING_SENDER_ID'],
+    appId: ['REACT_APP_FIREBASE_APP_ID'],
+    measurementId: ['REACT_APP_FIREBASE_MEASUREMENT_ID']
+};
+
+const firebaseConfig = Object.fromEntries(
+    Object.entries(envSources).map(([configKey, candidates]) => {
+        const value = candidates.map((name) => process.env[name]).find(Boolean);
+        return [configKey, value];
+    })
+);
+
+const requiredConfigKeys = [
+    'apiKey',
+    'authDomain',
+    'projectId',
+    'storageBucket',
+    'messagingSenderId',
+    'appId'
+];
+
+const missingEnvVars = requiredConfigKeys
+    .map((configKey) => envSources[configKey])
+    .map((candidates) => candidates.find((name) => Boolean(process.env[name])) ?? candidates[0])
+    .filter((envName) => !process.env[envName]);
+
+if (missingEnvVars.length > 0) {
+    throw new Error(
+        `Missing Firebase environment variables: ${missingEnvVars.join(', ')}. ` +
+        'Check your .env file or Vercel project settings.'
+    );
 }
 
 const app = initializeApp(firebaseConfig);
